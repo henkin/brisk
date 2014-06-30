4901e1780abafba8d278c4378c93b6f75a64b094
Pavel Henkin <talktopaul@gmail.com>
2014-06-29 19:09:00 -0700

    Cleaning up solution

---
+++ Brisk.Tests/EventPublisherTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+
+namespace Brisk.Tests
+{
+    [TestFixture]
+    public class EventPublisherTests
+    {
+        [Test]
+        public void CreateWebAndWorkerNodes()
+        {
+            Host worker = Host.Create();
+            Host web = Host.Create();
+            worker.Init();
+            web.Init();
+
+            object recievedEvent = null;
+            worker.Eventer.OnEvent((ev) => { recievedEvent = ev; });
+            
+            web.Commander.Create(new TestEntity(DateTime.Now));
+            
+            Thread.Sleep(500);
+            Assert.IsNotNull(recievedEvent, "Didn't get event in time");
+            //Assert.AreEqual(web.Repository<TestEntity>.Count(), 1);
+        }
+    }
+
+    public class TestEntity
+    {
+        public TestEntity(DateTime now)
+        {
+            
+        }
+    }
+}
